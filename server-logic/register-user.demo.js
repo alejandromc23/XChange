757bb3f833abccdf434d9b68f933f11dfdd7c812
Alejandro Mascort colomer <alejandro.mascort17@hotmail.com>
2020-09-30 11:42:18 +0200

    add data packages

--- server-logic/register-user.demo.js
+++ server-logic/register-user.demo.js
@@ -8,8 +8,8 @@ const registerUser = require('./register-user')
 
 mongoose.connect(MONGODB_URL)
     .then(async () => {
-        await registerUser('alejandro', 'mascort', 'alejandromascort@hotmail.com', '123123123')
-        console.log('user already registered')
+        await registerUser('alejandro', 'mascort', 'alejandro.mascort@hotmail.com', '123123123')
+        console.log('user registered')
     })
     .catch(error => console.log(error.message))
     .finally(() => mongoose.disconnect())
