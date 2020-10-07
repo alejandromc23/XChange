96e4af9ec51ec995defdde7d5e7edff0267d50e5
Alejandro Mascort colomer <alejandro.mascort17@hotmail.com>
2020-10-07 12:33:43 +0200

    fix error handling in register route

--- api/routes/handlers/register-user.js
+++ api/routes/handlers/register-user.js
@@ -1,5 +1,5 @@
 const { registerUser } = require('server-logic')
-const handleError = require('../../helpers')
+const {handleError} = require('../../helpers')
 
 module.exports = (req, res) => {
     const { body : { name, surname, email, password } } = req
@@ -7,8 +7,8 @@ module.exports = (req, res) => {
     try {
         registerUser(name, surname, email, password)
             .then(() => res.status(201).send())
-            .catch(handleError)
+            .catch(error => handleError(error, res))
     } catch (error) {
-        handleError(error)
+        handleError(error, res)
     }
 }
